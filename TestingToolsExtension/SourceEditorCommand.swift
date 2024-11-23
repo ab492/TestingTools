ec33ea0619cca9de834a49f886d7d248b78a2377
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-11-23 23:07:32 +0000

    . e add TestingToolsExtension

---
+++ TestingToolsExtension/SourceEditorCommand.swift
@@ -0,0 +1,19 @@
+//
+//  SourceEditorCommand.swift
+//  TestingToolsExtension
+//
+//  Created by Andy Brown on 23/11/2024.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
---
+++ TestingToolsExtension/SourceEditorExtension.swift
@@ -0,0 +1,26 @@
+//
+//  SourceEditorExtension.swift
+//  TestingToolsExtension
+//
+//  Created by Andy Brown on 23/11/2024.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorExtension: NSObject, XCSourceEditorExtension {
+    
+    /*
+    func extensionDidFinishLaunching() {
+        // If your extension needs to do any work at launch, implement this optional method.
+    }
+    */
+    
+    /*
+    var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
+        // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
+        return []
+    }
+    */
+    
+}
