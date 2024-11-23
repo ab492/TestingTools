073dacb75ed02988a29b3bef0eabf2e52cef6ef0
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-11-23 23:06:19 +0000

    . e setup initial project

---
+++ TestingTools/TestingToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingToolsApp.swift
+//  TestingTools
+//
+//  Created by Andy Brown on 23/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestingToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
