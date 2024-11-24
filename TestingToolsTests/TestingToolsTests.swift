128fd92e05f274d9857daa7f930f2729bd98700f
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-11-24 08:15:06 +0000

    . e add testing target to main project

---
+++ TestingToolsTests/TestingToolsTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestingToolsTests.swift
+//  TestingToolsTests
+//
+//  Created by Andy Brown on 24/11/2024.
+//
+
+import Testing
+
+struct TestingToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
