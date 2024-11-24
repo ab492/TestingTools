d31edc34e7186fe87f9871d074ef1728910735e6
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-11-24 08:02:45 +0000

    . e add demo project

---
+++ DemoProject/TestingToolsDemoProject/TestingToolsDemoProject/TestingToolsDemoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingToolsDemoProjectApp.swift
+//  TestingToolsDemoProject
+//
+//  Created by Andy Brown on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestingToolsDemoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
