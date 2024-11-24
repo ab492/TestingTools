8c909948583badced0a7983b0806839e439f8f06
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-11-24 08:06:21 +0000

    . e add xcode project and testing target for demo project

---
+++ TestingToolsDemoProject/TestingToolsDemoProjectTests/TestingToolsDemoProjectTests.swift
@@ -0,0 +1,16 @@
+//
+//  TestingToolsDemoProjectTests.swift
+//  TestingToolsDemoProjectTests
+//
+//  Created by Andy Brown on 24/11/2024.
+//
+
+import Testing
+
+struct TestingToolsDemoProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
