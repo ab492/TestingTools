63508537ad9e7ccf8cd16e814dd1dc4de4a431a8
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-11-28 06:55:51 +0000

    - f update createStruct code to use selected text

--- TestingToolsTests/TestingToolsTests.swift
+++ TestingToolsTests/TestingToolsTests.swift
@@ -9,15 +9,25 @@ import Testing
 import XcodeKit
 @testable import TestingToolsExtension
 
-func createStruct(allText: [String]) -> String {
-    "struct TestStruct { }"
+func createStruct(allText: [String], selectedText: XCSourceTextRange) -> String {
+    let line = allText[selectedText.start.line]
+    
+    let startIndex = line.index(line.startIndex, offsetBy: selectedText.start.column)
+    let endIndex = line.index(line.startIndex, offsetBy: selectedText.end.column)
+    let selectedWord = String(line[startIndex..<endIndex])
+    
+    return "struct \(selectedWord) { }"
 }
 
 struct TestingToolsTests {
     @Test func oneWord_withThatWordSelected_correctlyCreatesStruct() {
         let text = ["TestStruct"]
+        let selectedText = XCSourceTextRange(
+            start: XCSourceTextPosition(line: 0, column: 0),
+            end: XCSourceTextPosition(line: 0, column: 10)
+        )
         
-        let sut = createStruct(allText: text)
+        let sut = createStruct(allText: text, selectedText: selectedText)
         
         #expect(sut == "struct TestStruct { }")
     }
