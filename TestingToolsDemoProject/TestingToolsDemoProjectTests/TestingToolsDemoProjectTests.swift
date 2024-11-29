28cd18d7c810ffe444b372ffef6b8ee98941a360
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-11-29 06:51:54 +0000

    - f integrate create struct with source editor command

--- TestingToolsDemoProject/TestingToolsDemoProjectTests/TestingToolsDemoProjectTests.swift
+++ TestingToolsDemoProject/TestingToolsDemoProjectTests/TestingToolsDemoProjectTests.swift
@@ -1,16 +1,2 @@
-//
-//  TestingToolsDemoProjectTests.swift
-//  TestingToolsDemoProjectTests
-//
-//  Created by Andy Brown on 24/11/2024.
-//
-
-import Testing
-
-struct TestingToolsDemoProjectTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-    let sut = MyStruct()
-}
+        let sut = MyStruct()
+struct MyStruct { }
--- TestingToolsExtension/SourceEditorCommand.swift
+++ TestingToolsExtension/SourceEditorCommand.swift
@@ -12,40 +12,15 @@ class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         let buffer = invocation.buffer
         let selections = buffer.selections as! [XCSourceTextRange]
+        let allLines = buffer.lines as! [String]
         
-        var selectedText = ""
-        
-        for selection in selections {
-            let startLine = selection.start.line
-            let startColumn = selection.start.column
-            let endLine = selection.end.line
-            let endColumn = selection.end.column
-            
-            
-            // Handle single-line and multi-line selections
-            if startLine == endLine {
-                // Single-line selection
-                if let line = buffer.lines[startLine] as? String {
-                    let range = line.index(line.startIndex, offsetBy: startColumn)..<line.index(line.startIndex, offsetBy: endColumn)
-                    selectedText += String(line[range])
-                }
+        do {
+            if let maybeNewStruct = try createStruct(allText: allLines, selectedText: selections) {
+                buffer.lines.add(maybeNewStruct)
+                completionHandler(nil)
             }
+        } catch {
+            completionHandler(error)
         }
-        
-        print("SELECTED TEXT: \(selectedText)")
-
-        
-        
-        //
-        let lines = invocation.buffer.lines
-        let stringLines = lines as! [String]
-        let createType = CreateType()
-        let newLines = createType.createStruct(from: "")
-        newLines.forEach {
-            lines.add($0)
-        }
-        
-        completionHandler(nil)
     }
-    
 }
