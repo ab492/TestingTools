09c967479e1e33cafe1d77779ef719c548773d4e
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-12-06 14:01:42 +0000

    - f add actions enum to represent available actions and hook up create class code

--- TestingToolsExtension/SourceEditorCommand.swift
+++ TestingToolsExtension/SourceEditorCommand.swift
@@ -8,15 +8,43 @@
 import Foundation
 import XcodeKit
 
+enum Action: String, CaseIterable {
+    case createStruct = "testingtools.createStruct"
+    case createClass = "testingtools.createClass"
+    
+    var identifier: String {
+        self.rawValue
+    }
+    
+    var name: String {
+        switch self {
+        case .createStruct: return "Create Struct"
+        case .createClass: return "Create Class"
+        }
+    }
+}
+
 class SourceEditorCommand: NSObject, XCSourceEditorCommand {
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        
+        guard let action = Action(rawValue: invocation.commandIdentifier) else {
+            return completionHandler(nil)
+        }
+        
         let buffer = invocation.buffer
         let selections = buffer.selections as! [XCSourceTextRange]
         let allLines = buffer.lines as! [String]
         
         do {
-            if let maybeNewStruct = try createStruct(allText: allLines, selectedText: selections) {
-                buffer.lines.add(maybeNewStruct)
+            let textToAdd: String?
+            
+            switch action {
+            case .createStruct: textToAdd = try createStruct(allText: allLines, selectedText: selections)
+            case .createClass: textToAdd = try createClass(allText: allLines, selectedText: selections)
+            }
+            
+            if let textToAdd {
+                buffer.lines.add(textToAdd)
                 completionHandler(nil)
             }
         } catch {
