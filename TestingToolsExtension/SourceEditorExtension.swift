1699d0302aebdfc1d361445371588a58c4110693
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-12-06 14:03:11 +0000

    . e remove command definitions from info.plist as they're now set in code

--- TestingToolsExtension/SourceEditorExtension.swift
+++ TestingToolsExtension/SourceEditorExtension.swift
@@ -9,19 +9,11 @@ import Foundation
 import XcodeKit
 
 class SourceEditorExtension: NSObject, XCSourceEditorExtension {
-    
-    /*
-    func extensionDidFinishLaunching() {
-        // If your extension needs to do any work at launch, implement this optional method.
-    }
-    */
-    
     var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
         return Action.allCases
             .map { [XCSourceEditorCommandDefinitionKey.identifierKey: $0.identifier,
                     XCSourceEditorCommandDefinitionKey.nameKey: $0.name,
                     XCSourceEditorCommandDefinitionKey.classNameKey: SourceEditorCommand.className()
-                ]}
+            ]}
     }
-    
 }
