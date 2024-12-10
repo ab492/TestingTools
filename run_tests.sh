44d72ce869c2256414f90502b0feab217f20d226
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-12-10 07:09:28 +0000

    . e add CODE_SIGNING_ALLOWED=NO to run_tests.sh for remote builds

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-xcodebuild test -project TestingTools.xcodeproj -scheme TestingToolsExtension -destination 'platform=macOS,arch=x86_64'
+xcodebuild test \
+  -project TestingTools.xcodeproj \
+  -scheme TestingToolsExtension \
+  -destination 'platform=macOS,arch=x86_64' \
+  CODE_SIGNING_ALLOWED=NO
