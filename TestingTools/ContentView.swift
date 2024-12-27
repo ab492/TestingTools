1fdd949ef27d6ef15932cdde52aebdbb0888d28b
Andy Brown <35888770+ab492@users.noreply.github.com>
2024-12-27 06:51:15 +0000

    - f add basic instructions to app

--- TestingTools/ContentView.swift
+++ TestingTools/ContentView.swift
@@ -1,13 +1,39 @@
 import SwiftUI
 
+import AppKit
+
+func openExtensionsPane() {
+    guard let url = URL(string: "x-apple.systempreferences:com.apple.preference.extensions") else {
+        return
+    }
+    NSWorkspace.shared.open(url)
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(.testingToolsIcon)
-                .resizable()
-                .scaledToFit().frame(width: 100, height: 100)
-            Text("Testing Tools")
-                .font(.largeTitle)
+        VStack(spacing: 20) {
+            VStack {
+                Image(.testingToolsIcon)
+                    .resizable()
+                    .scaledToFit().frame(width: 100, height: 100)
+                Text("Testing Tools")
+                    .font(.largeTitle)
+            }
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("How to Enable")
+                        .font(.headline)
+                    Text("")
+                    Button("Open System Settings") {
+                        openExtensionsPane()
+                    }
+                    Text("• Select 'General'")
+                    Text("• Navigate to 'Login Items & Extensions'")
+                    Text("• Select 'Xcode Source Editor'")
+                    Text("• Enable 'Testing Tools'")
+                }
+                Spacer()
+            }
         }
         .padding()
     }
