5c9439c707ec8d831d86a7acb0f2d9643730549d
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-05 06:42:38 +0000

    - f add tab width to command action handler

--- TestingToolsExtension/Action Handler/CommandActionHandler.swift
+++ TestingToolsExtension/Action Handler/CommandActionHandler.swift
@@ -2,7 +2,12 @@ import Foundation
 import XcodeKit
 
 struct CommandActionHandler {
-    static func handle(action: Action, allText: [String], selections: [XCSourceTextRange]) throws -> [String] {
+    static func handle(
+        action: Action,
+        allText: [String],
+        selections: [XCSourceTextRange],
+        tabWidth: Int
+    ) throws -> [String] {
         switch action {
         case .createLocalProperty:
             return try createProperty(allText: allText, selectedText: selections)
