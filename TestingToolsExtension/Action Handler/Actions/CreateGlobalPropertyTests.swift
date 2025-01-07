e4a7e439a6961775e40a26d9161ebfe6224df2d9
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-07 06:43:58 +0000

    - f

--- TestingToolsExtension/Action Handler/Actions/CreateGlobalPropertyTests.swift
+++ TestingToolsExtension/Action Handler/Actions/CreateGlobalPropertyTests.swift
@@ -72,7 +72,26 @@ struct CreateGlobalPropertyTests {
             try makeSut(allText: text, selections: [])
         }
     }
-
+    
+    func multipleSelectedText_throwsError() {
+        let text = [
+            "struct TestStruct {\n",
+            "    someProperty.callSomeMethod()\n",
+            "}\n"
+        ]
+        let firstSelection = XCSourceTextRange(
+            start: XCSourceTextPosition(line: 0, column: 0),
+            end: XCSourceTextPosition(line: 0, column: 3)
+        )
+        let secondSelection = XCSourceTextRange(
+            start: XCSourceTextPosition(line: 0, column: 5),
+            end: XCSourceTextPosition(line: 0, column: 10)
+        )
+        
+        #expect(throws: TestingToolsError.multipleSelectionNotSupported) {
+            try makeSut(allText: text, selections: [firstSelection, secondSelection])
+        }
+    }
 }
 
 private func makeSut(allText: [String], selections: [XCSourceTextRange]) throws -> [String] {
