e1e79749a25888a7fa669f61f61932b7ff4f8e77
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-07 07:44:05 +0000

    . t update make sut

--- TestingToolsExtension/Action Handler/Actions/CreateGlobalPropertyTests.swift
+++ TestingToolsExtension/Action Handler/Actions/CreateGlobalPropertyTests.swift
@@ -17,7 +17,11 @@ struct CreateGlobalPropertyTests {
         ]
         let highlightedText = getRangeOfText("someProperty", from: text)!
 
-        let sut = try makeSut(allText: text, selections: [highlightedText])
+        let sut = try makeSut(
+            action: .createGlobalProperty,
+            allText: text,
+            selections: [highlightedText]
+        )
         
         #expect(sut == [
             "import SomeLibrary\n",
@@ -46,7 +50,11 @@ struct CreateGlobalPropertyTests {
         ]
         let highlightedText = getRangeOfText("someProperty", from: text)!
 
-        let sut = try makeSut(allText: text, selections: [highlightedText])
+        let sut = try makeSut(
+            action: .createGlobalProperty,
+            allText: text,
+            selections: [highlightedText]
+        )
         
         #expect(sut == [
             "let someProperty = \u{003C}#Type#\u{003E}\n",
@@ -69,7 +77,11 @@ struct CreateGlobalPropertyTests {
         ]
         
         #expect(throws: TestingToolsError.invalidSelection) {
-            try makeSut(allText: text, selections: [])
+            try makeSut(
+                action: .createGlobalProperty,
+                allText: text,
+                selections: []
+            )
         }
     }
     
@@ -89,7 +101,11 @@ struct CreateGlobalPropertyTests {
         )
         
         #expect(throws: TestingToolsError.multipleSelectionNotSupported) {
-            try makeSut(allText: text, selections: [firstSelection, secondSelection])
+            try makeSut(
+                action: .createGlobalProperty,
+                allText: text,
+                selections: [firstSelection, secondSelection]
+            )
         }
     }
     
@@ -105,7 +121,11 @@ struct CreateGlobalPropertyTests {
         )
         
         #expect(throws: TestingToolsError.multilineSelectionNotSupported) {
-            try makeSut(allText: text, selections: [multipleLineSelection])
+            try makeSut(
+                action: .createGlobalProperty,
+                allText: text,
+                selections: [multipleLineSelection]
+            )
         }
     }
     
@@ -123,12 +143,16 @@ struct CreateGlobalPropertyTests {
         )
         
         #expect(throws: TestingToolsError.invalidSelection) {
-            try makeSut(allText: text, selections: [outOfBoundsSelection])
+            try makeSut(
+                action: .createGlobalProperty,
+                allText: text,
+                selections: [outOfBoundsSelection]
+            )
         }
     }
 }
 
-private func makeSut(allText: [String], selections: [XCSourceTextRange]) throws -> [String] {
+private func makeSut(action: Action, allText: [String], selections: [XCSourceTextRange]) throws -> [String] {
     try CommandActionHandler.handle(
         action: .createGlobalProperty,
         allText: allText,
