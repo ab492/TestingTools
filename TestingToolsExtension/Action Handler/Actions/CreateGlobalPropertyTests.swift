b92e34fc8ca8a98a43e4280b665b92127ff9d6a6
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-08 06:35:39 +0000

    . t tidy up property tests to use parametrized testing for error handling

--- TestingToolsExtension/Action Handler/Actions/CreateGlobalPropertyTests.swift
+++ TestingToolsExtension/Action Handler/Actions/CreateGlobalPropertyTests.swift
@@ -2,184 +2,193 @@ import Testing
 import XcodeKit
 
 struct CreateGlobalPropertyTests {
-
-    @Test func testCreatingLocalProperty() throws {
-        let text = [
-            "struct TestStruct {\n",
-            "    struct SomeNestedStruct {\n",
-            "        func someDummyMethod() {\n",
-            "            someProperty.callSomeMethod()\n",
-            "        }\n",
-            "    }\n",
-            "}\n"
-        ]
-        let highlightedText = getRangeOfText("someProperty", from: text)!
-
-        
-        let sut = try makeSut(
-            action: .createLocalProperty,
-            allText: text,
-            selections: [highlightedText]
-        )
-        
-        #expect(sut == [
-            "struct TestStruct {\n",
-            "    struct SomeNestedStruct {\n",
-            "        func someDummyMethod() {\n",
-            "            let someProperty = \u{003C}#Type#\u{003E}\n",
-            "            someProperty.callSomeMethod()\n",
-            "        }\n",
-            "    }\n",
-            "}\n"
-        ])
-    }
     
-    
-    @Test func testCreatingGlobalPropertyWhenThereAreImportStatements() throws {
-        let text = [
-            "import SomeLibrary\n",
-            "\n",
-            "struct TestStruct {\n",
-            "    struct SomeNestedStruct {\n",
-            "        func someDummyMethod() {\n",
-            "            someProperty.callSomeMethod()\n",
-            "        }\n",
-            "    }\n",
-            "}\n"
-        ]
-        let highlightedText = getRangeOfText("someProperty", from: text)!
+    struct LocalPropertyTests {
+        @Test func testCreatingLocalProperty() throws {
+            let text = [
+                "struct TestStruct {\n",
+                "    struct SomeNestedStruct {\n",
+                "        func someDummyMethod() {\n",
+                "            someProperty.callSomeMethod()\n",
+                "        }\n",
+                "    }\n",
+                "}\n"
+            ]
+            let highlightedText = getRangeOfText("someProperty", from: text)!
 
-        let sut = try makeSut(
-            action: .createGlobalProperty,
-            allText: text,
-            selections: [highlightedText]
-        )
-        
-        #expect(sut == [
-            "import SomeLibrary\n",
-            "\n",
-            "let someProperty = \u{003C}#Type#\u{003E}\n",
-            "\n",
-            "struct TestStruct {\n",
-            "    struct SomeNestedStruct {\n",
-            "        func someDummyMethod() {\n",
-            "            someProperty.callSomeMethod()\n",
-            "        }\n",
-            "    }\n",
-            "}\n"
-        ])
-    }
-    
-    @Test func testCreatingGlobalPropertyWhenThereAreNoImportStatements() throws {
-        let text = [
-            "struct TestStruct {\n",
-            "    struct SomeNestedStruct {\n",
-            "        func someDummyMethod() {\n",
-            "            someProperty.callSomeMethod()\n",
-            "        }\n",
-            "    }\n",
-            "}\n"
-        ]
-        let highlightedText = getRangeOfText("someProperty", from: text)!
-
-        let sut = try makeSut(
-            action: .createGlobalProperty,
-            allText: text,
-            selections: [highlightedText]
-        )
-        
-        #expect(sut == [
-            "let someProperty = \u{003C}#Type#\u{003E}\n",
-            "\n",
-            "struct TestStruct {\n",
-            "    struct SomeNestedStruct {\n",
-            "        func someDummyMethod() {\n",
-            "            someProperty.callSomeMethod()\n",
-            "        }\n",
-            "    }\n",
-            "}\n"
-        ])
-    }
-    
-    @Test func testErrorIsThrownIfNoSelectionIsMade() {
-        let text = [
-            "struct TestStruct {\n",
-            "    someProperty.callSomeMethod()\n",
-            "}\n"
-        ]
-        
-        #expect(throws: TestingToolsError.invalidSelection) {
-            try makeSut(
-                action: .createGlobalProperty,
+            
+            let sut = try makeSut(
+                action: .createLocalProperty,
                 allText: text,
-                selections: []
+                selections: [highlightedText]
             )
+            
+            #expect(sut == [
+                "struct TestStruct {\n",
+                "    struct SomeNestedStruct {\n",
+                "        func someDummyMethod() {\n",
+                "            let someProperty = \u{003C}#Type#\u{003E}\n",
+                "            someProperty.callSomeMethod()\n",
+                "        }\n",
+                "    }\n",
+                "}\n"
+            ])
         }
     }
     
-    @Test func multipleSelectedText_throwsError() {
-        let text = [
-            "struct TestStruct {\n",
-            "    someProperty.callSomeMethod()\n",
-            "}\n"
-        ]
-        let firstSelection = XCSourceTextRange(
-            start: XCSourceTextPosition(line: 0, column: 0),
-            end: XCSourceTextPosition(line: 0, column: 3)
-        )
-        let secondSelection = XCSourceTextRange(
-            start: XCSourceTextPosition(line: 0, column: 5),
-            end: XCSourceTextPosition(line: 0, column: 10)
-        )
-        
-        #expect(throws: TestingToolsError.multipleSelectionNotSupported) {
-            try makeSut(
+    struct GlobalPropertyTests {
+        @Test func testCreatingGlobalPropertyWhenThereAreImportStatements() throws {
+            let text = [
+                "import SomeLibrary\n",
+                "\n",
+                "struct TestStruct {\n",
+                "    struct SomeNestedStruct {\n",
+                "        func someDummyMethod() {\n",
+                "            someProperty.callSomeMethod()\n",
+                "        }\n",
+                "    }\n",
+                "}\n"
+            ]
+            let highlightedText = getRangeOfText("someProperty", from: text)!
+
+            let sut = try makeSut(
                 action: .createGlobalProperty,
                 allText: text,
-                selections: [firstSelection, secondSelection]
+                selections: [highlightedText]
             )
+            
+            #expect(sut == [
+                "import SomeLibrary\n",
+                "\n",
+                "let someProperty = \u{003C}#Type#\u{003E}\n",
+                "\n",
+                "struct TestStruct {\n",
+                "    struct SomeNestedStruct {\n",
+                "        func someDummyMethod() {\n",
+                "            someProperty.callSomeMethod()\n",
+                "        }\n",
+                "    }\n",
+                "}\n"
+            ])
+        }
+        
+        @Test func testCreatingGlobalPropertyWhenThereAreNoImportStatements() throws {
+            let text = [
+                "struct TestStruct {\n",
+                "    struct SomeNestedStruct {\n",
+                "        func someDummyMethod() {\n",
+                "            someProperty.callSomeMethod()\n",
+                "        }\n",
+                "    }\n",
+                "}\n"
+            ]
+            let highlightedText = getRangeOfText("someProperty", from: text)!
+
+            let sut = try makeSut(
+                action: .createGlobalProperty,
+                allText: text,
+                selections: [highlightedText]
+            )
+            
+            #expect(sut == [
+                "let someProperty = \u{003C}#Type#\u{003E}\n",
+                "\n",
+                "struct TestStruct {\n",
+                "    struct SomeNestedStruct {\n",
+                "        func someDummyMethod() {\n",
+                "            someProperty.callSomeMethod()\n",
+                "        }\n",
+                "    }\n",
+                "}\n"
+            ])
         }
     }
     
-    @Test func multipleLineSelectedText_throwsError() {
-        let text = [
-            "struct TestStruct {\n",
-            "    someProperty.callSomeMethod()\n",
-            "}\n"
-        ]
-        let multipleLineSelection = XCSourceTextRange(
-            start: XCSourceTextPosition(line: 0, column: 0),
-            end: XCSourceTextPosition(line: 1, column: 10)
-        )
-        
-        #expect(throws: TestingToolsError.multilineSelectionNotSupported) {
-            try makeSut(
-                action: .createGlobalProperty,
-                allText: text,
-                selections: [multipleLineSelection]
-            )
+    struct ErrorHandling {
+        @Test(arguments: [Action.createLocalProperty, .createGlobalProperty])
+        func testErrorIsThrownIfNoSelectionIsMade(action: Action) {
+            let text = [
+                "struct TestStruct {\n",
+                "    someProperty.callSomeMethod()\n",
+                "}\n"
+            ]
+            
+            #expect(throws: TestingToolsError.invalidSelection) {
+                try makeSut(
+                    action: action,
+                    allText: text,
+                    selections: []
+                )
+            }
         }
-    }
-    
-    @Test func testOutOfBoundsLineIndex_throwsError() {
-        let text = [
-            "struct TestStruct {\n",
-            "    someProperty.callSomeMethod()\n",
-            "}\n"
-        ]
         
-        // Notice "line: 4" is out of range
-        let outOfBoundsSelection = XCSourceTextRange(
-            start: XCSourceTextPosition(line: 4, column: 0),
-            end: XCSourceTextPosition(line: 4, column: 3)
-        )
-        
-        #expect(throws: TestingToolsError.invalidSelection) {
-            try makeSut(
-                action: .createGlobalProperty,
-                allText: text,
-                selections: [outOfBoundsSelection]
+        @Test(arguments: [Action.createLocalProperty, .createGlobalProperty])
+        func multipleSelectedText_throwsError(action: Action) {
+            let text = [
+                "struct TestStruct {\n",
+                "    someProperty.callSomeMethod()\n",
+                "}\n"
+            ]
+            let firstSelection = XCSourceTextRange(
+                start: XCSourceTextPosition(line: 0, column: 0),
+                end: XCSourceTextPosition(line: 0, column: 3)
             )
+            let secondSelection = XCSourceTextRange(
+                start: XCSourceTextPosition(line: 0, column: 5),
+                end: XCSourceTextPosition(line: 0, column: 10)
+            )
+            
+            #expect(throws: TestingToolsError.multipleSelectionNotSupported) {
+                try makeSut(
+                    action: action,
+                    allText: text,
+                    selections: [firstSelection, secondSelection]
+                )
+            }
+        }
+        
+        @Test(arguments: [Action.createLocalProperty, .createGlobalProperty])
+        func multipleLineSelectedText_throwsError(action: Action) {
+            let text = [
+                "struct TestStruct {\n",
+                "    someProperty.callSomeMethod()\n",
+                "}\n"
+            ]
+            let multipleLineSelection = XCSourceTextRange(
+                start: XCSourceTextPosition(line: 0, column: 0),
+                end: XCSourceTextPosition(line: 1, column: 10)
+            )
+            
+            #expect(throws: TestingToolsError.multilineSelectionNotSupported) {
+                try makeSut(
+                    action: action,
+                    allText: text,
+                    selections: [multipleLineSelection]
+                )
+            }
+        }
+        
+        @Test(arguments: [Action.createLocalProperty, .createGlobalProperty])
+        func testOutOfBoundsLineIndex_throwsError(action: Action) {
+            let text = [
+                "struct TestStruct {\n",
+                "    someProperty.callSomeMethod()\n",
+                "}\n"
+            ]
+            
+            // Notice "line: 4" is out of range
+            let outOfBoundsSelection = XCSourceTextRange(
+                start: XCSourceTextPosition(line: 4, column: 0),
+                end: XCSourceTextPosition(line: 4, column: 3)
+            )
+            
+            #expect(throws: TestingToolsError.invalidSelection) {
+                try makeSut(
+                    action: action,
+                    allText: text,
+                    selections: [outOfBoundsSelection]
+                )
+            }
         }
     }
 }
