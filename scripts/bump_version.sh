cc6579c66ae557b51a2427e00c9fed52745f554e
Andy Brown <35888770+ab492@users.noreply.github.com>
2025-01-16 14:21:54 +0000

    add work version script

---
+++ scripts/bump_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# WORK IN PROGRESS SCRIPT
+
+set -e # Bail if any steps fail
+
+echo "Bumping version number..."
+
+# Path to your Info.plist
+INFO_PLIST="../TestingTools/Info.plist"
